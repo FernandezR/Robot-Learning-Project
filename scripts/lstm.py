4306bb91cb78f4dc830b454558e738c0663fb94b
rcorona <rcorona@utexas.edu>
2016-11-08 00:08:03 -0600

    Updated lstm script to incorporate folds.;

--- scripts/lstm.py
+++ scripts/lstm.py
@@ -5,66 +5,112 @@ from keras.models import Sequential
 from keras.layers import LSTM, RepeatVector
 import os
 import preprocess
+import sys
 
-#Fixed random seed for reproducability. 
-numpy.random.seed(10)
+def evaluate_with_meteor(results_file_name, ref_file_name, test_file_name):
+    results_file = open(results_file_name, 'r')
+    ref_file = open(ref_file_name, 'w')
+    test_file = open(test_file_name, 'w')
 
-#Sequence batch length. 
-traj_seq_len = 18
-word_seq_len = 18
-num_h = 50
-traj_dim = 8
+    #Gets all files and encodes them for METEOR. 
+    for line in results_file:
+        prediction, ground_truth = line.split('::::')
 
-#Get training data. 
-dirs = []
-path = '../dataset/robobarista_dataset/dataset/' 
+        #Clean up formatting. 
+        prediction = prediction.strip('. ').strip() + '.'
+        ground_truth = ground_truth.strip()
 
-#Extract all directories. 
-for file_name in os.listdir(path):
-    if os.path.isdir(path + file_name):
-        dirs.append(file_name)
+        #Write it to reference and test files. 
+        test_file.write(prediction.encode('UTF-8') + u'\n')
+        ref_file.write(ground_truth.encode('UTF-8') + u'\n')
 
-train = dirs[:20]
-test = dirs[20:40]
+    ref_file.close()
+    test_file.close()
 
-train = preprocess.load_data_set(path, train)
-test = preprocess.load_data_set(path, test)
+def run_lstm(fold_num):
+    #Fixed random seed for reproducability. 
+    numpy.random.seed(10)
 
-train_l = train[2]
-train_t = train[0]
-original_text = train_l
+    #Sequence batch length. 
+    traj_seq_len = 18
+    word_seq_len = 18
+    num_h = 50
+    traj_dim = 8
 
-vocab = preprocess.create_vocabulary(train_l)
-train_l = preprocess.to_one_hot(vocab, train_l, word_seq_len)
+    #Get training data. 
+    dirs = []
+    path = '../dataset/robobarista_dataset/dataset/' 
 
-#Pad trajectories. 
-train_t = preprocess.pad_trajectory_sequences(train_t, traj_seq_len)
+    folds = preprocess.get_folds_dictionary(path + 'folds.json')
 
-#Instantiate model. 
-model = Sequential()
+    train = [name.encode('ascii') for name in folds[fold_num]['train']]
+    test = [name.encode('ascii') for name in folds[fold_num]['test']]
 
-#LSTM for encoding trajectories into fixed dimensionality vector. 
-model.add(LSTM(num_h, input_dim=traj_dim, input_length=traj_seq_len, return_sequences=False))
+    train = preprocess.load_data_set(path, train)
+    test = preprocess.load_data_set(path, test)
 
-#Hands encoder output to decoder. 
-model.add(RepeatVector(traj_seq_len))
+    #Splits train and test into their parts. 
+    train_t, train_p, train_l = train
+    test_t, test_p, test_l = test
 
-#Decoder for taking trajectory vectors and generating natural language descriptions. 
-model.add(LSTM(len(vocab.keys()), input_dim=num_h, input_length=word_seq_len, return_sequences=True))
+    #Prepare vocabulary and create one-hot vectors for training set. 
+    vocab = preprocess.create_vocabulary(train_l)
+    train_l = preprocess.to_one_hot(vocab, train_l, word_seq_len)
 
-#Compiles model for use. 
-model.compile(loss='mse', optimizer='rmsprop')
+    #Pad trajectories. 
+    train_t = preprocess.pad_trajectory_sequences(train_t, traj_seq_len)
+    test_t = preprocess.pad_trajectory_sequences(test_t, traj_seq_len)
 
-#Trains model. 
-model.fit(train_t, train_l, nb_epoch = 1000, batch_size = 80)
+    #Instantiate model. 
+    model = Sequential()
 
-#Predicts from training. 
-predictions =  model.predict_classes(train_t, batch_size = 1)
+    #LSTM for encoding trajectories into fixed dimensionality vector. 
+    model.add(LSTM(num_h, input_dim=traj_dim, input_length=traj_seq_len, return_sequences=False))
 
-print predictions
+    #Hands encoder output to decoder. 
+    model.add(RepeatVector(traj_seq_len))
 
-#Converts predictions to phrases in natural language. 
-predictions = preprocess.class_vectors_to_phrases(predictions, vocab)
+    #Decoder for taking trajectory vectors and generating natural language descriptions. 
+    model.add(LSTM(len(vocab.keys()), input_dim=num_h, input_length=word_seq_len, return_sequences=True))
 
-for i in range(len(predictions)):
-    print predictions[i] + ' :::: ' + original_text[i]
+    #Compiles model for use. 
+    model.compile(loss='mse', optimizer='rmsprop')
+
+    #Trains model. 
+    model.fit(train_t, train_l, nb_epoch = 1000, batch_size = 80)
+
+    #Predicts from training. 
+    predictions =  model.predict_classes(test_t, batch_size = 1)
+
+    #Converts predictions to phrases in natural language. 
+    predictions = preprocess.class_vectors_to_phrases(predictions, vocab)
+
+    results_file = open('lstm_fold_' + str(fold_num) + '_test.txt', 'w')
+
+    for i in range(len(predictions)):
+        results_file.write(predictions[i] + ' :::: ' + test_l[i] + '\n')
+
+    results_file.close()
+
+def print_usage():
+    print 'Run lstm training: ./lstm.py run_lstm [fold_num]'
+    print 'Evaluate LSTM results file using METEOR: ./lstm.py evaluate [results_file] [ref_file] [test_file]'
+
+if __name__ == '__main__':
+    if not len(sys.argv) >= 2:
+        print_usage()
+
+    elif sys.argv[1] == 'run_lstm':
+        if not len(sys.argv) == 3:
+            print_usage()
+        else:
+            run_lstm(int(sys.argv[2]))
+
+    elif sys.argv[1] == 'evaluate':
+        if len(sys.argv) == 5:
+            evaluate_with_meteor(sys.argv[2], sys.argv[3], sys.argv[4])
+        else:
+            print_usage()
+
+    else:
+        print_usage()
